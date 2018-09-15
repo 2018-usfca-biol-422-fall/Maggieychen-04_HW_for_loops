6b242bbb41a323d6b1483dc4e976bf066b93cdab
Maggie Chen <ychen254@dons.usfca.edu>
2018-09-14 19:16:30 -0700

    Initial command script, descriptions of script

---
+++ code/02_count_sequences.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# A script to count the number of sequence
+# in an anbitary number of a fasta file
+# and report that information to the user
+# Initially created by Maggie Chen
+# ychen254@dons.usfca.edu
+# 2018-09-14
+
+# expecting a fasta file formated line like this:
+
+# >sequence_id
+# AGCTCGTC.....
+
+# This will get the ">" of each sequence_id in fasta files (grep ">")
+# then count the number of ">" as the number of sequence (the wc -l)
